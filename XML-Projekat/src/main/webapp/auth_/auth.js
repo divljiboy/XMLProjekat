abe8c33f7950b383d4a5d61b9abca09df0483539
kerkezn <kerkez144@gmail.com>
2016-06-09 17:04:32 +0200

    Izmenjena struktura foldera za logovanje, registraciju i odjavljivanje

---
+++ XML-Projekat/src/main/webapp/auth_/auth.js
@@ -0,0 +1,31 @@
+/**
+ * Created by David on 6/9/2016.
+ */
+
+
+(function(angular){
+
+    angular.module("xmlApp").config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+
+        $stateProvider
+            .state('signIn', {
+                url: '/signin',
+                views: {
+                    "content":{
+                        templateUrl:'auth_/signin_/signIn.html'
+                    }
+                }
+            })
+            .state('signOut', {
+                url: '/signout',
+                views: {
+                    "content":{
+                        templateUrl:'signout_/signOut.html'
+                    }
+                }
+            })
+
+    }]);
+
+})(angular);
